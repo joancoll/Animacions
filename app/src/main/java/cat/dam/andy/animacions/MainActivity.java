30cf855cfde64a0d8f58f53197cb6373fc759741
joancoll <joancoll@inspladelestany.cat>
2023-02-27 19:57:58 +0100

    gradle update and minor changes

--- app/src/main/java/cat/dam/andy/animacions/MainActivity.java
+++ app/src/main/java/cat/dam/andy/animacions/MainActivity.java
@@ -1,110 +1,107 @@
 package cat.dam.andy.animacions;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView iv_mosquit;
-    AnimationDrawable mosquit_animat;
-    ConstraintLayout main_screen;
+    ImageView iv_mosquito;
+    AnimationDrawable animated_mosquito;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        animacioOu();
-        animacioOu2();
-        animacioMosquit();
-        animacioPilota1();
-        animacioPilota2();
-        animacioPilota3();
-        animacioPilota4();
-
+        init_animations();
     }
 
-    private void animacioOu() {
-        ImageView iv_ou =(ImageView) findViewById(R.id.iv_ou);
-        iv_ou.setOnClickListener(v -> {
+    private void init_animations() {
+        anim_egg_chicken();
+        anim_egg_falling();
+        anim_ball_appears();
+        anim_ball_rolls();
+        anim_ball_bounces();
+        anim_ball_rolls_comes();
+        anim_mosquito_flying();
+    }
+
+    private void anim_egg_chicken() {
+        ImageView iv_egg =(ImageView) findViewById(R.id.iv_egg1);
+        iv_egg.setOnClickListener(v -> {
             // Canviem la imatge assignant-li l'altre recurs
-            ImageView visorImatge = (ImageView) findViewById(R.id.iv_ou);
-            visorImatge.setImageResource(R.drawable.gall);
+            iv_egg.setImageResource(R.drawable.chicken);
         });
     }
 
-    private void animacioOu2() {
-        ImageView iv_ou =(ImageView) findViewById(R.id.iv_ou2);
-        iv_ou.setOnClickListener(v -> {
+    private void anim_egg_falling() {
+        ImageView iv_egg =(ImageView) findViewById(R.id.iv_egg2);
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_falling);
+        iv_egg.setOnClickListener(v -> {
             // Canviem la imatge assignant-li l'altre recurs
-            ImageView visorImatge = (ImageView) findViewById(R.id.iv_ou);
-            Animation animacioPilota = AnimationUtils.loadAnimation(MainActivity.this, R.anim.ouquecau);
-            iv_ou.startAnimation(animacioPilota);
+            iv_egg.startAnimation(anim);
         });
     }
 
-    private void animacioPilota1() {
-        final ImageView iv_pilota = (ImageView)findViewById(R.id.iv_pilota1);
-        iv_pilota.setOnClickListener(v -> {
+    private void anim_ball_appears() {
+        final ImageView iv_ball1 = (ImageView)findViewById(R.id.iv_ball1);
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_appears);
+        iv_ball1.setOnClickListener(v -> {
             // Canviem la imatge assignant-li l'altre recurs
-            Animation animacioPilota = AnimationUtils.loadAnimation(MainActivity.this, R.anim.apareixer);
-            iv_pilota.startAnimation(animacioPilota);
+            iv_ball1.startAnimation(anim);
         });
     }
 
-    private void animacioPilota2() {
-        final ImageView iv_pilota = (ImageView)findViewById(R.id.iv_pilota2);
-        iv_pilota.setOnClickListener(v -> {
+    private void anim_ball_rolls() {
+        final ImageView iv_ball2 = (ImageView)findViewById(R.id.iv_ball2);
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_rolls);
+        iv_ball2.setOnClickListener(v -> {
             // Canviem la imatge assignant-li l'altre recurs
-            Animation animacioPilota = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rodar);
-            iv_pilota.startAnimation(animacioPilota);
+            iv_ball2.startAnimation(anim);
         });
     }
 
-    private void animacioPilota3() {
-        final ImageView iv_pilota = (ImageView)findViewById(R.id.iv_pilota3);
-        iv_pilota.setOnClickListener(v -> {
+    private void anim_ball_bounces() {
+        final ImageView iv_ball3 = (ImageView)findViewById(R.id.iv_ball3);
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_bounces);
+        iv_ball3.setOnClickListener(v -> {
             // Canviem la imatge assignant-li l'altre recurs
-            Animation animacioPilota = AnimationUtils.loadAnimation(MainActivity.this, R.anim.botar);
-            iv_pilota.startAnimation(animacioPilota);
+            iv_ball3.startAnimation(anim);
         });
     }
 
-    private void animacioPilota4() {
-        final ImageView iv_pilota = (ImageView)findViewById(R.id.iv_pilota4);
-        iv_pilota.setOnClickListener(v -> {
+    private void anim_ball_rolls_comes() {
+        final ImageView iv_ball4 = (ImageView)findViewById(R.id.iv_ball4);
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_rolls_comes);
+        iv_ball4.setOnClickListener(v -> {
             // Canviem la imatge assignant-li l'altre recurs
-            Animation animacioPilota = AnimationUtils.loadAnimation(MainActivity.this, R.anim.venir);
-            iv_pilota.startAnimation(animacioPilota);
+            iv_ball4.startAnimation(anim);
         });
     }
 
-    private void animacioMosquit() {
-        mosquit_animat = new AnimationDrawable();
+    private void anim_mosquito_flying() {
+        animated_mosquito = new AnimationDrawable();
         // Carrega el ImageView que contindrà l'animació i actualiza el fons d'imatge amb el recurs XML on es defineix les imatges
         // i temps d'animació del mosquit
-        iv_mosquit = (ImageView) findViewById(R.id.iv_mosquit);
+        iv_mosquito = (ImageView) findViewById(R.id.iv_mosquit);
         // Situem la imatge en la pantalla
-        iv_mosquit.setX(100);
-        iv_mosquit.setY(150);
-        iv_mosquit.setBackgroundResource(R.drawable.mosquit_animat);
+        iv_mosquito.setX(100);
+        iv_mosquito.setY(150);
+        iv_mosquito.setBackgroundResource(R.drawable.animation_mosquito);
         // Obté el fons que ha estat compilat amb un objecte AnimationDrawable
-        mosquit_animat = (AnimationDrawable) iv_mosquit.getBackground();
+        animated_mosquito = (AnimationDrawable) iv_mosquito.getBackground();
         // Comença l'animació (per defecte repetició de cicle).
-        mosquit_animat.start();
-        iv_mosquit.setOnClickListener(view -> {
+        animated_mosquito.start();
+        iv_mosquito.setOnClickListener(view -> {
             // En cas de que es cliqui el mosquit actualiza el fons d'imatge amb el recurs XML on es defineix les imatges
             // i temps d'animació de la taca de sang
-            iv_mosquit.setBackgroundResource(R.drawable.sang_animat);
-            mosquit_animat = (AnimationDrawable) iv_mosquit.getBackground();
+            iv_mosquito.setBackgroundResource(R.drawable.animation_blood);
+            animated_mosquito = (AnimationDrawable) iv_mosquito.getBackground();
             // Fes l'animació (només un cicle).
-            mosquit_animat.start();
+            animated_mosquito.start();
         });
     }
 
